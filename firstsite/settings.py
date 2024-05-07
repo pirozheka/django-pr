3a3925dfb5162f00e1de3be300c58f913d612ed7
pirozheka <pirozheka@gmail.com>
2024-05-07 18:59:29 +0500

    форма фильтрации

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.flatpages',
     'fpages',
     'simpleapp',
+    'django_filters',
 ]
 
 SITE_ID = 1
--- simpleapp/views.py
+++ simpleapp/views.py
@@ -6,6 +6,7 @@ from datetime import datetime
 # что в этом представлении мы будем выводить список объектов из БД
 from django.views.generic import ListView, DetailView
 from .models import Product
+from .filters import ProductFilter
 
 
 class ProductsList(ListView):
@@ -13,7 +14,21 @@ class ProductsList(ListView):
     ordering = 'name'
     template_name = 'products.html'
     context_object_name = 'products'
+    paginate_by = 2
 
+    # Переопределяем функцию получения списка товаров
+    def get_queryset(self):
+       # Получаем обычный запрос
+       queryset = super().get_queryset()
+       # Используем наш класс фильтрации.
+       # self.request.GET содержит объект QueryDict, который мы рассматривали
+       # в этом юните ранее.
+       # Сохраняем нашу фильтрацию в объекте класса,
+       # чтобы потом добавить в контекст и использовать в шаблоне.
+       self.filterset = ProductFilter(self.request.GET, queryset)
+       # Возвращаем из функции отфильтрованный список товаров
+       return self.filterset.qs
+    
     # Метод get_context_data позволяет нам изменить набор данных,
     # который будет передан в шаблон.
     def get_context_data(self, **kwargs):
@@ -26,7 +41,11 @@ class ProductsList(ListView):
         context['time_now'] = datetime.now()
         # Добавим ещё одну пустую переменную,
         # чтобы на её примере рассмотреть работу ещё одного фильтра.
-        context['next_sale'] = 'None'
+        context['next_sale'] = None
+        
+       # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset
+
         return context
 
 class ProductDetail(DetailView):
